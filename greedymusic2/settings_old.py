f81df047ea3835a52e0f8ffc53211c22f7e6df0e
kashsingh <max.payne61@gmail.com>
2016-07-12 00:15:31 +0530

    final changes

--- greedymusic2/settings_old.py
+++ greedymusic2/settings_old.py
@@ -16,6 +16,7 @@ from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 TEMPLATE_CONTEXT_PROCESSORS += (
     'django.core.context_processors.request',
 )
